e49f2c17d4d3d1030ad31444bf6b1089f20d529e
André Nonaka <nonaka.andre@gmail.com>
2026-05-30 16:34:06 -0300

    criando nav

--- admin.php
+++ admin.php
@@ -2,16 +2,43 @@
 
 ?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-br" data-bs-theme="dark">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>Área administrativa</title>
 </head>
 
 <body>
-    <h1 style="color: red; font-size: 54px;"> TESTE DE CONEXÃO COM ÁREA ADMINISTRATIVA</h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <img src="image/vamoscasar.svg" alt="logo" width="150px">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item col-3">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item col-5">
+                        <a class="nav-link" href="#">Adicionar lista</a>
+                    </li>
+                    <li class="nav-item col-4">
+                        <a class="nav-link" href="#">Recebidos</a>
+                    </li>
+                    <li class="nav-item col-4">
+                        <a class="nav-link" href="#">Mensagens</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
